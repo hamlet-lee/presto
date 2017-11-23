d49ef5c8658e36771b238bd68ac233cee4a0b1f0
Hamlet Lee <lisining@gmail.com>
2017-11-23 17:48:57 +0800

    MOD: try to fix CatalogSchemaName case problem

--- presto-spi/src/main/java/com/facebook/presto/spi/CatalogSchemaName.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/CatalogSchemaName.java
@@ -26,7 +26,8 @@ public final class CatalogSchemaName
     public CatalogSchemaName(String catalogName, String schemaName)
     {
         this.catalogName = requireNonNull(catalogName, "catalogName is null").toLowerCase(ENGLISH);
-        this.schemaName = requireNonNull(schemaName, "schemaName is null").toLowerCase(ENGLISH);
+        //this.schemaName = requireNonNull(schemaName, "schemaName is null").toLowerCase(ENGLISH);
+        this.schemaName = requireNonNull(schemaName, "schemaName is null");
     }
 
     public String getCatalogName()
